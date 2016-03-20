9621585c65beae8649eb05bd2e0226379d293f7c
nozagleh <nozagleh@gmail.com>
2016-03-20 12:28:04 +0100

    basic create,read interation with db

--- python/models.py
+++ python/models.py
@@ -3,38 +3,53 @@ from pymongo import MongoClient
 
 class Connector:
 	#initilize variables
-	#servername, portnr, dbname, collectionname, client, db, collection
 
 	#define init method with no arguments
 
 	#define init method that takes servername and portnr as arguments
 	def __init__(self, servername = 'localhost', portnr = 27017, dbname = 'test', collectionname = 'testcollection'):
-		#set servername and portnr
-		self.servername = servername
-		self.portnr = portnr
-		self.dbname = dbname
-		self.collectionname = collectionname
+		print(servername + ";" + str(portnr) + ";" + dbname + ";" + collectionname + ";")
+
+		global client
+		global db
+		global collection
+
 		try:
 			client = MongoClient(servername,portnr)
-			db = dbname
-			collection = collectionname
-		except Exception, e:
-			print("problem")
+			db = client['test']
+			collection = db['testcollection']
+			print(db.collection.count())
+		except pymongo.errors.ConnectionFailure, e:
+			print("problem connecting to MongoDB: %s" % e)
 		finally:
 			print("connected to mongodb")
 		
 
 	#create the four pillars of db based programming, CRUD
-	def create():
-		return 0
-	def read():
-		return 0
+
+	def create(self):
+		#define data to insert
+		v = {'x':42}
+		#insert into collection
+		collection.insert(v)
+
+	def read(self):
+		#read the data from collection
+		data = collection.find()
+
+		#iterate through the data and print to console
+		for row in data:
+			print(row)
+	
 	def update():
 		return 0
+	
 	def delete(): 
 		return 0
 
 x = Connector()
+print(x.read())
+#print(x.create())
 
 
 """MongoDB stores data in JSON format. MongoDB is a nosql language, meaning it is not relational. The structure is documentational,
